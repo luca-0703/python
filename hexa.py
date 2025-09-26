3ff81ab172023569742afe64e35acb95f06c06de
luca-0703 <luca07032007@gmail.com>
2025-09-26 10:56:29 +0200

    Sur la branche master  Votre branche est à jour avec 'origin/master'.  Modifications qui seront validées : 	modifié :         hexa.py

---
+++ hexa.py
@@ -0,0 +1,17 @@
+#On commence par demander le numero de telephone en decimal 
+print("Rentré votre numero de télphone ")
+num_dec = int(input("Votre numero :"))
+#On vas maintenant definir les numero qui depasse les 9 en hexa
+table_hexa  = {
+    0: "0", 1: "1", 2: "2", 3: "3", 4: "4",
+    5: "5", 6: "6", 7: "7", 8: "8", 9: "9",
+    10: "A", 11: "B", 12: "C", 13: "D", 14: "E", 15: "F"
+}
+#On vas maintenant faire la conversion
+
+while 0 > 
+
+
+
+
+
